6a9db77437678a168f14466a8cc84ea394fa9218
turonov <turonov5400gmail.com>
2022-10-20 17:07:33 +0500

    1

--- if01.py
+++ if01.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
+    if a > b:
+        if a > c:
+            return a
+        else:return c
+    else:
+        if b > c:
+            return b
+        else: return a
     return
+
+print(main(15, 14, 10))    
